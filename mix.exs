a9844cce8d46da1d15e1c1d233f3dd608401f0c8
dxt <doxtop@synrc.com>
2021-04-25 02:34:12 +0300

    init

---
+++ mix.exs
@@ -0,0 +1,15 @@
+defmodule Idu.MixProject do
+  use Mix.Project
+
+  def project, do: [
+      app: :idu,
+      description: "Nitro Elements",
+      version: "0.1.0",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+  ]
+
+  def application, do: [ extra_applications: [] ]
+  defp deps, do: []
+end
---
+++ src/idu.erl
@@ -0,0 +1,8 @@
+-module(idu).
+-behaviour(application).
+-behaviour(supervisor).
+-export([start/2, stop/1, init/1]).
+
+start(_,_) -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+stop(_) -> ok.
+init([]) -> {ok, {{one_for_one, 5, 10}, []}}.
